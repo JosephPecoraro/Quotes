e6082c9d7c8ebc6f348cdd1ae300e1c468072d39
Joseph Pecoraro <joepeck02@gmail.com>
2009-05-30 22:05:15 -0400

    Added some validation.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,7 @@ class Quote < ActiveRecord::Base
   belongs_to :author
 	has_and_belongs_to_many :tags
 	validates_presence_of :text, :author_id
+	validates_associated :tags
 	after_update :save_tags
 	after_destroy :check_author
 
